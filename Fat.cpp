a45d8d6ca268ba98b2fc83a538a4d7db4eeb9df6
Liu Linhong <linhong.liu@intel.com>
2014-10-22 12:25:21 +0800

    Handle non-format partition in Vold
    fsck_msdos will retun error code 8 when the partition is non-format.
    Handle this error code and continue next partition in Vold.
    
    Change-Id: Id1561209593bf3994ac0cf5c85b049281fe7c723
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-4634
    Signed-off-by: Liu Linhong <linhong.liu@intel.com>
    Category: aosp improvement
    Domain: USB-Host
    Origin: internal
    Upstream-Candidate: yes
    Reviewed-on: https://android.intel.com:443/291662

--- Fat.cpp
+++ Fat.cpp
@@ -102,6 +102,11 @@ int Fat::check(const char *fsPath) {
             errno = EIO;
             return -1;
 
+        case 8:
+            SLOGE("Filesystem check failed (no filesystem)");
+            errno = ENODATA;
+            return -1;
+
         default:
             SLOGE("Filesystem check failed (unknown exit code %d)", status);
             errno = EIO;
